e14e39b0f0cfbc966b7c28e5fbdc7608e8600791
Ian Russell <IanThomasR@gmail.com>
2022-07-18 19:19:42 -0400

    Update run_setup.sh

--- .setup/run_setup.sh
+++ .setup/run_setup.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
-echo "########################################################"
-echo "######                                           #######"
-echo "######     Would you like to install extras?     #######"
-echo "######                   Y/n                     #######"
-echo "######                                           #######"
-echo "########################################################"
 
-read ANS
-echo
-echo "Updating..."
 sudo apt update && sudo apt upgrade -y
-echo "Finished!"
-echo "Building directories..."
+
 # Add directories
-cd
+cd ~
 mkdir -p WorkBench
-mkdir -p WorkBench/GitHub
+mkdir -p WorkBench/repos
 mkdir -p WorkBench/pyenvs
 mkdir -p WorkBench/sandbox
 mkdir -p WorkBench/sandbox/analysis
 mkdir -p WorkBench/sandbox/scrap
 
-echo "Cloning dotfiles..."
 # Git Configuration
 GITUSER="ITRussell";
 git config --global user.name "${GITUSER}"
 GITEMAIL="IanThomasR@gmail.com";
 git config --global user.email "${GITEMAIL}"
-
 
 # And that your source repository ignores the folder where you'll clone it, so that you don't create weird recursion problems:
 echo ".cfg" >> .gitignore
@@ -41,24 +29,14 @@ mkdir -p .config-backup && \
 xargs -I{} mv {} .config-backup/{}
 
 # Checkout
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout Ubuntu
 # Set to not show untracked files
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
-
-# Set system preferences
-echo
-echo "Setting system preferences (GNOME)"
-dconf load / < ~/.config/dconf-settings.ini
-
 
 # Packages
 echo
 echo "Installing software..."
+sudo add-apt-repository ppa:mmstick76/alacritty
 curl -fsSL https://starship.rs/install.sh | bash
-xargs -a ~/.config/packages.list sudo apt install -y -qq 
-
-
-echo
-echo "Setting up tools..."
-# Tools
+sudo apt install $(cat ~/.config/packages.list) -y
 
