ebe4ca917869b63874ea2c63c35bac980475c4bd
Ian Russell <IanThomasR@gmail.com>
2022-07-19 19:01:24 -0400

    Updated

--- .setup/run_setup.sh
+++ .setup/run_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# SYSTEM UPDATE
 sudo apt update && sudo apt upgrade -y
 
-# Add directories
+# DIRECTORIES
 cd ~
 mkdir -p WorkBench
 mkdir -p WorkBench/repos
@@ -11,12 +12,39 @@ mkdir -p WorkBench/sandbox
 mkdir -p WorkBench/sandbox/analysis
 mkdir -p WorkBench/sandbox/scrap
 
-# Git Configuration
+# GIT CONFIG
 GITUSER="ITRussell";
 git config --global user.name "${GITUSER}"
 GITEMAIL="IanThomasR@gmail.com";
 git config --global user.email "${GITEMAIL}"
 
+# SOFTWARE
+# apt
+sudo apt install $(cat ~/.config/packages.list) -y
+
+# Alacritty & Starship
+curl -fsSL https://starship.rs/install.sh | bash # Starship prompt
+git clone https://github.com/alacritty/alacritty.git 
+cd alacritty
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env && rustup default nightly && rustup update
+sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 gzip
+cargo build --release
+sudo cp target/release/alacritty /usr/local/bin
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+sudo mkdir -p /usr/local/share/man/man1
+gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+mkdir -p $fish_complete_path[1]
+cp extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
+mkdir -p ~/.bash_completion
+cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
+echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
+rm -r $HOME/Alacritty
+
+# DOT FILES
 # And that your source repository ignores the folder where you'll clone it, so that you don't create weird recursion problems:
 echo ".cfg" >> .gitignore
 
@@ -33,12 +61,5 @@ xargs -I{} mv {} .config-backup/{}
 # Set to not show untracked files
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
-# Packages
-echo
-echo "Installing software..."
-sudo add-apt-repository ppa:mmstick76/alacritty
-sudo apt update
-sudo apt install alacritty
-curl -fsSL https://starship.rs/install.sh | bash
-sudo apt install $(cat ~/.config/packages.list) -y
+
 
