8a0709994643dcba03be1234e38defc3cd82a8a5
Ian Russell <IanThomasR@gmail.com>
2022-07-19 20:24:44 -0400

    Update run_setup.sh

--- .setup/run_setup.sh
+++ .setup/run_setup.sh
@@ -23,7 +23,22 @@ git config --global user.name "${GITUSER}"
 GITEMAIL="IanThomasR@gmail.com";
 git config --global user.email "${GITEMAIL}"
 
+# DOT FILES
+# And that your source repository ignores the folder where you'll clone it, so that you don't create weird recursion problems:
+echo ".cfg" >> .gitignore
 
+# Clone dot files repo
+git clone --bare https://github.com/itrussell/dotfiles.git $HOME/.cfg
+
+# backup any existing configs
+mkdir -p .config-backup && \
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+
+# Checkout
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout Ubuntu
+# Set to not show untracked files
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
 # Alacritty & Starship
 curl -fsSL https://starship.rs/install.sh | bash # Starship prompt
@@ -46,22 +61,7 @@ mkdir -p ~/.bash_completion
 cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
 echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
 
-# DOT FILES
-# And that your source repository ignores the folder where you'll clone it, so that you don't create weird recursion problems:
-echo ".cfg" >> .gitignore
 
-# Clone dot files repo
-git clone --bare https://github.com/itrussell/dotfiles.git $HOME/.cfg
-
-# backup any existing configs
-mkdir -p .config-backup && \
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} .config-backup/{}
-
-# Checkout
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout Ubuntu
-# Set to not show untracked files
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
 
 
